7bd2098680e9c9e16d834a60e9aa6c30d03511ed
crist <cjpires@uta.cv>
2025-09-01 22:35:16 -0100

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // para gerar site estático
   images: {
     unoptimized: true, // desabilita a otimização de imagens
     remotePatterns: [
