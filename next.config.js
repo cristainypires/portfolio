27fe093dc9f029979d2863bf8b565166ff9fe980
crist <cjpires@uta.cv>
2025-09-01 22:56:43 -0100

    a

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // desabilita a otimização de imagens
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -12,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
